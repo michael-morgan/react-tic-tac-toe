97096c9d786d83ff109ac701412c824fe1cb23f5
Michael Morgan <personal.michaelmorgan@gmail.com>
2017-02-09 11:36:28 -0500

    Finished rest of logic for React Tic Tac Toe. It's now in a playable state.

--- components/Board/Board.js
+++ components/Board/Board.js
@@ -12,7 +12,8 @@ class Board extends React.Component {
   constructor() {
     super();
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     };
   }
 
@@ -21,7 +22,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+
+    let status;
+    status = winner ? 'Winner: ' + winner
+                    : 'Next player: '
+                    + (this.state.xIsNext ? 'X' : 'O');
+
     return (
       <div>
         <div className={cx(s.status, this.props.className)}>{status}</div>
@@ -46,9 +53,36 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+
+    if(calculateWinner(squares) || squares[i]) { return; }
+
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default Board;
--- components/Square/Square.js
+++ components/Square/Square.js
@@ -2,19 +2,12 @@ import React, { PropTypes } from 'react';
 import cx from 'classnames';
 import s from './Square.css';
 
-class Square extends React.Component {
-
-  static propTypes = {
-    className: PropTypes.string,
-  };
-
-  render() {
-    return (
-      <button className={cx(s.square, this.props.className)} onClick={this.props.onClick}>
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className={s.square} onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 export default Square;
